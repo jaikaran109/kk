2a4b1b24b41431b3312679fb03f9e263bf447b98
jaikaran109 <jaik05581@gmail.com>
2025-08-29 11:25:07 +0530

    Create loop.java

---
+++ Lec 2/loop.java
@@ -0,0 +1,57 @@
+public class loopL9 {
+    public static void main(String[] args) {
+
+        /*
+         * syntax of for loops:
+         * 
+         * for(initialize; condition ; increment/decrement){
+         *       body
+         * }
+         */
+
+        //  Q: Print number from 1 to 5
+        // for(int num=1 ; num<=5;num++){
+        //     System.out.println(num);
+        // }
+
+        // Scanner input = new Scanner(System.in);
+        // int  n = input.nextInt();
+        // for(int i = 1; i <= n ;i++){
+        //     System.out.print(i+" ");
+        // }
+
+
+
+        //while loop
+        /*
+         * syntax
+         * while(condition){
+         *      body
+         * }
+         */
+
+
+
+        // int i = 1;
+        // while(i<=5){
+        //     System.out.println(i);
+        //     i++;
+        // }
+
+
+
+        //do while loop
+        /*
+         * do{
+         * 
+         * }while(condition);
+         */
+
+
+        int n = 1;
+        do{
+            System.out.println(n);
+            n++;
+        }while(n<=5);
+    }
+}
